23324f5930ae96f77d0507ab71fd736f8cf68dcd
Guido Bergman <gbergman@fi.uba.ar>
2024-05-19 22:27:55 -0300

    Fix data preprocess

--- bash_scripts/preprocess_semeval.sh
+++ bash_scripts/preprocess_semeval.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-SEMEVAL_DATA_PATH=""
+SEMEVAL_DATA_PATH="data/original/data"
+OUTPUT_PATH="data"
 
-cp -r ${SEMEVAL_DATA_PATH}/*/train-articles-subtask-3/* train/articles
-cp -r ${SEMEVAL_DATA_PATH}/*/train-labels-subtask-3-spans/* train/labels
+mkdir ${OUTPUT_PATH}/train/
+mkdir ${OUTPUT_PATH}/dev/
+mkdir ${OUTPUT_PATH}/train/articles
+mkdir ${OUTPUT_PATH}/dev/articles
+mkdir ${OUTPUT_PATH}/train/labels
+mkdir ${OUTPUT_PATH}/dev/labels
 
-cp -r ${SEMEVAL_DATA_PATH}/*/dev-articles-subtask-3/* dev/articles
-cp -r ${SEMEVAL_DATA_PATH}/*/dev-labels-subtask-3-spans/* dev/labels
+
+cp -r ${SEMEVAL_DATA_PATH}/*/train-articles-subtask-3/*  ${OUTPUT_PATH}/train/articles
+cp -r ${SEMEVAL_DATA_PATH}/*/train-labels-subtask-3-spans/* ${OUTPUT_PATH}/train/labels
+
+cp -r ${SEMEVAL_DATA_PATH}/*/dev-articles-subtask-3/* ${OUTPUT_PATH}/dev/articles
+cp -r ${SEMEVAL_DATA_PATH}/*/dev-labels-subtask-3-spans/* ${OUTPUT_PATH}/dev/labels
 
 # Delete duplicated articles
-find train/articles -type f -name '*(*).txt'  -delete
-find dev/articles -type f -name '*(*).txt'  -delete
+find ${OUTPUT_PATH}/train/articles -type f -name '*(*).txt'  -delete
+find ${OUTPUT_PATH}/dev/articles -type f -name '*(*).txt'  -delete
